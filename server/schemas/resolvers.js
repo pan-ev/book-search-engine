2e3a369652f9a0fc3ed9658e632d9eda9a94ad24
epan <epan616@gmail.com>
2023-06-25 16:52:19 -0400

    Update TypeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,15 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Book, User } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        
+    }
+
+
+}
+
 
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,33 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+const typeDefs = gql`
 
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login: Auth
+        
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+`;
 
 module.exports = typeDefs;
