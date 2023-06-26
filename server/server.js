b430ba2101c1d9f20678589611bca89adced6f09
epan <epan616@gmail.com>
2023-06-25 21:57:44 -0400

    fix bugs

--- server/server.js
+++ server/server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
