0642cfa15c5557d959d71c1947fb99b24cb3bb2a
epan <epan616@gmail.com>
2023-06-25 22:09:37 -0400

    Update env

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
