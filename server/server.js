9aee0bcea4b81adc4353cc3b61c4721487fa5af6
epan <epan616@gmail.com>
2023-07-05 11:19:23 -0700

    remove node modules and package-lock

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,8 +23,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-const startApolloServer = async () => {
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   
@@ -35,4 +39,4 @@ const startApolloServer = async () => {
   })
   };
 
-startApolloServer();
+startApolloServer(typeDefs, resolvers);
